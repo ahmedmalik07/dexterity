8c81bfd5b543bae1fcd0ee376e1c5455c6e6aab6
Ayesha Irfan <aishairfan017@gmail.com>
2026-09-12 16:25:09 +0500

    insert function

--- scripts/assets.cjs
+++ scripts/assets.cjs
@@ -3,3 +3,4 @@ const path=require('node:path');
 const env={...process.env};delete env.ELECTRON_RUN_AS_NODE;
 const child=spawn(require('electron'),[path.join(__dirname,'render-icon.cjs')],{env,windowsHide:true,stdio:'inherit'});
 child.on('exit',code=>process.exit(code||0));
+//script.js
